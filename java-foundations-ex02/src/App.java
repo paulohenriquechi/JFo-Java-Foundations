cd96a28c22bd45118d2aa8750ad8e362e9e61a72
Paulo <paulo.henrique.chi@outlook.com>
2022-10-31 12:31:16 -0300

    Exercício 02

---
+++ java-foundations-ex02/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Problema 1: Arte ASCII Simples");
+        System.out.println("  ####  ");
+        System.out.println(" #    # ");
+        System.out.println("# #  # #");
+        System.out.println("#      #");
+        System.out.println("# #  # #");
+        System.out.println("#  ##  #");
+        System.out.println(" #    # ");
+        System.out.println("  ####  ");
+        System.out.println(" ");
+        System.out.println("Problema 2: Arte ASCII Original");
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\ ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)  ");
+    }
+}
